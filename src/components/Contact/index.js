d03ce2630623228f2f52fef38d4d77df49014abd
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2022-02-06 02:55:58 -0600

    convert contact form to text

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,66 +3,75 @@ import { validateEmail } from '../../utils/helpers';
 
 
 function ContactForm() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    // const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-    const { name, email, message } = formState;
+    // const { name, email, message } = formState;
 
-    const [errorMessage, setErrorMessage] = useState('');
+    // const [errorMessage, setErrorMessage] = useState('');
 
 
-    function handleChange(e) {
-        if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-            console.log(isValid);
-            if (!isValid) {
-                setErrorMessage('Your email is invalid');
-            } else {
-                setErrorMessage('');
-            }
-        }
-        else {
-            if (!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`);
-            }
-            else {
-                setErrorMessage('');
-            }
-        }
-        if (!errorMessage) {
-            setFormState({...formState, [e.target.name]: e.target.value });
-        }
-    }
+    // function handleChange(e) {
+    //     if (e.target.name === 'email') {
+    //         const isValid = validateEmail(e.target.value);
+    //         console.log(isValid);
+    //         if (!isValid) {
+    //             setErrorMessage('Your email is invalid');
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     }
+    //     else {
+    //         if (!e.target.value.length) {
+    //             setErrorMessage(`${e.target.name} is required.`);
+    //         }
+    //         else {
+    //             setErrorMessage('');
+    //         }
+    //     }
+    //     if (!errorMessage) {
+    //         setFormState({...formState, [e.target.name]: e.target.value });
+    //     }
+    // }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
-    }
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     console.log(formState);
+    // }
 
-    return(
-    <section className='contact'>
-        <h2>Contact me</h2>
-        <form id='contact-form' onSubmit={handleSubmit}>
-            <div className='form-name'>
-                <label htmlFor='name'>Name:</label>
-                <input type="text" defaultValue={name} onBlur={handleChange} name="name" />            
-            </div>
-            <div className='form-email'>
-                <label htmlFor='email'>Email address:</label>
-                <input type="email" defaultValue={email} name="email" onBlur={handleChange} />            
-            </div>
-            <div className='form-message'>
-                <label htmlFor='message'>Message:</label>
-                <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />            
-            </div>
-            {errorMessage && (
-                <div>
-                    <p className='error-text'>{errorMessage}</p>
-                </div>
-            )}
-            <button type='submit'>Submit</button>
-        </form>
-    </section>
-    );
+    // return(
+    // <section className='contact'>
+    //     <h2>Contact me</h2>
+    //     <form id='contact-form' onSubmit={handleSubmit}>
+    //         <div className='form-name'>
+    //             <label htmlFor='name'>Name:</label>
+    //             <input type="text" defaultValue={name} onBlur={handleChange} name="name" />            
+    //         </div>
+    //         <div className='form-email'>
+    //             <label htmlFor='email'>Email address:</label>
+    //             <input type="email" defaultValue={email} name="email" onBlur={handleChange} />            
+    //         </div>
+    //         <div className='form-message'>
+    //             <label htmlFor='message'>Message:</label>
+    //             <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />            
+    //         </div>
+    //         {errorMessage && (
+    //             <div>
+    //                 <p className='error-text'>{errorMessage}</p>
+    //             </div>
+    //         )}
+    //         <button type='submit'>Submit</button>
+    //     </form>
+    // </section>
+    // );
+
+    return (
+        <section className='contact'>
+            <h2>Contact me</h2>
+            <p>Email: <a href="mailto:allyson.r.mcgrath@gmail.com">allyson.r.mcgrath@gmail.com</a></p>
+            <p>Phone: (970)310-9075</p>
+            <p>LinkedIn: <a href='https://www.linkedin.com/in/allyson-mcgrath/'>allyson-mcgrath</a></p>
+        </section>
+    )
 }
 
 export default ContactForm;
